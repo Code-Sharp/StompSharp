e7899fbc25536d21cf25e6a64a594675ae9dd3be
Shani Elharrar <shani.elha@gmail.com>
2014-05-13 21:13:26 +0300

    Added Documentation to IMessageRouter.

--- StompSharp/IMessageRouter.cs
+++ StompSharp/IMessageRouter.cs
@@ -5,8 +5,18 @@ namespace Stomp2
     public interface IMessageRouter
     {
 
+        /// <summary>
+        /// Gets all the messages with the specific given
+        /// <paramref name="command"/>
+        /// </summary>
+        /// <param name="command">A command to filter the messages by</param>
+        /// <returns></returns>
         IObservable<IMessage> GetObservable(string command);
 
+        /// <summary>
+        /// Gets all the messages that are routed via this
+        /// instance of <see cref="IMessageRouter"/>.
+        /// </summary>
         IObservable<IMessage> All { get; }
 
     }
